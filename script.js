a67612004bb634ac5ccb6156820883aded8aa101
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2026-03-30 15:48:29 +0500

    add animation in navbar

--- script.js
+++ script.js
@@ -190,3 +190,20 @@ function typeEffect() {
 }
 
 typeEffect();
+
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+});
